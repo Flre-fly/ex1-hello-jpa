4d43bb77b6f520aa7d4a7fd2b59b32b7f9932d7f
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2022-03-15 17:49:21 +0900

    회원등록

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,13 +2,30 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.transaction.Transaction;
 
 public class JpaMain {
     public static void main(String[] args){
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
 
+        //코드 추가
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("홍길동");
+
+            em.persist(member);
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }
         em.close();
         emf.close();
 
