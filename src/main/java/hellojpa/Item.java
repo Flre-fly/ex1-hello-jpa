627b25f9fbd67bd3ef5295adb4ec5969aab95f73
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2022-03-22 11:11:18 +0900

    상속관계매핑 - Inheritance 어노테이션이 없으면 기본적으로 한 테이블에 상속된 컬럼들이 몰빵된 형태로 테이블이 생성된다

---
+++ src/main/java/hellojpa/Item.java
@@ -0,0 +1,18 @@
+package hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+
+}
